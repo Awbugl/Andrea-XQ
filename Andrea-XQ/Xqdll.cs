0d24bf7c5d3c1e118ab336091c1f2b062574c6bd
littlenine12 <56431872+littlenine12@users.noreply.github.com>
2021-07-28 21:29:19 +0800

    Revert "Merge branch 'main' of https://github.com/littlenine12/Andrea-XQ into main"
    This reverts commit ab5caa3307ea158340aec9335d19dd460d683c7c, reversing
    changes made to 97b9b82133d1ff6f4591c2c9a314d46d3f5c3c1b.

---
+++ Andrea-XQ/Xqdll.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace AndreaBot.XQ
+{
+    internal static class Xqdll
+    {
+        private const string DllName = "xqapi.dll";
+        
+        [DllImport(DllName, CharSet = CharSet.Ansi, EntryPoint = "S3_Api_GetGroupAdmin")]
+        internal static extern IntPtr GetGroupAdmin(byte[] autoid, string robotQq, string group);
+
+        [DllImport(DllName, CharSet = CharSet.Ansi, EntryPoint = "S3_Api_HandleFriendEvent")]
+        internal static extern void HandleFriendEvent(byte[] autoid, string robotQq, string qq, int messageType,
+            string message);
+
+        [DllImport(DllName, CharSet = CharSet.Ansi, EntryPoint = "S3_Api_GetNick")]
+        internal static extern IntPtr GetNick(byte[] autoid, string robotQq, string qq);
+
+        [DllImport(DllName, CharSet = CharSet.Ansi, EntryPoint = "S3_Api_HandleGroupEvent")]
+        internal static extern void HandleGroupEvent(byte[] autoid, string robotQq, int requestType, string qq,
+            string group, string seq, int messageType, string message);
+
+        [DllImport(DllName, CharSet = CharSet.Ansi, EntryPoint = "S3_Api_QuitGroup")]
+        internal static extern void QuitGroup(byte[] autoid, string robotQq, string group);
+
+        [DllImport(DllName, CharSet = CharSet.Ansi, EntryPoint = "S3_Api_SendMsg")]
+        internal static extern void SendMsg(byte[] autoid, string robotQq, int messageType, string group, string qq,
+            string message, int bubbleId);
+
+        [DllImport(DllName, CharSet = CharSet.Ansi, EntryPoint = "S3_Api_SendXML")]
+        internal static extern void SendXML(byte[] autoid, string robotQq, int sendType, int messageType, string group,
+            string qq, string xmlMessage);
+        
+        [DllImport(DllName, CharSet = CharSet.Ansi, EntryPoint = "S3_Api_LoginQQ")]
+        public static extern void LoginQQ(byte[] autoid, string robotQq);
+
+        [DllImport(DllName, CharSet = CharSet.Ansi, EntryPoint = "S3_Api_OffLineQQ")]
+        public static extern void OffLineQQ(byte[] autoid, string robotQq);
+        
+        [DllImport(DllName, CharSet = CharSet.Ansi, EntryPoint = "S3_Api_GetQQList")]
+        public static extern IntPtr GetQQList(byte[] autoid);
+        
+        [DllImport(DllName, CharSet = CharSet.Ansi, EntryPoint = "S3_Api_GetOnLineList")]
+        public static extern IntPtr GetOnLineList(byte[] autoid);
+
+        [DllImport("kernel32.dll", EntryPoint = "GetProcessHeap")]
+        internal static extern int GetProcessHeap();
+
+        [DllImport("kernel32.dll", EntryPoint = "HeapFree")]
+        internal static extern int HeapFree(int hheap, int dwflags, IntPtr lpmeem);
+    }
+}
